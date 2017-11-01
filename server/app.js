3bb2916e129fa11b0f82e5d226f4312365680a23
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-01 20:21:13 +0700

    basic server setup using expressJS, body-parser, dotenv, and cors modules

---
+++ server/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`hactivoverflow server is listening on port ${port}`));
