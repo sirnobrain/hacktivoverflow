7c728e3b03402157ce9bfeeeefd54a5bb8ebaf3f
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-01 23:58:31 +0700

    create signup using username and password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,27 @@
 'use strict'
 
 const models = require('./../models');
+const generateResponse = require('./../helpers/generate-response');
+const generateJwtoken = require('./../helpers/generate-jwtoken');
 
 class Auth {
-  static signup(req, res) {}
+  static signup(req, res) {
+    const signupUser = { username: req.body.username, password: req.body.password };
+
+    models.User.create(signupUser)
+    .then(userCreated => {
+      const jwtoken = generateJwtoken(userCreated);
+      const payload = { username: userCreated.username, jwtoken: jwtoken };
+      const resp = generateResponse(200, 'user created', payload, null);
+
+      res.status(resp.status).send(resp);
+    })
+    .catch(err => {
+      const resp = generateResponse(500, 'failed to create user', null, err);
+
+      res.status(resp.status).send(resp);
+    });
+  }
 
   static signin(req, res) {}
 }
