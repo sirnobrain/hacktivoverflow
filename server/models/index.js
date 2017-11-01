51835a3ef376f69526b4081f1c0b1aa9882e1f85
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-01 21:15:15 +0700

    create question model

--- server/models/index.js
+++ server/models/index.js
@@ -3,8 +3,9 @@
 const mongoose = require('mongoose');
 
 const User = require('./user');
+const Question = require('./question');
 
 mongoose.connect(process.env.DB_HOST, { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
-module.exports = { User }
+module.exports = { User, Question }
