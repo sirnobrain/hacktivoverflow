d2de607542f6b6b83748c2769266e37ec77a373d
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-06 21:29:17 +0700

    modify app.js to include question and answer routes

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const auth = require('./routes/auth');
+const question = require('./routes/question');
+const answer = require('./routes/answer');
 
 const app = express();
 
@@ -16,6 +18,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/auth', auth);
+app.use('/question', question);
+app.use('/answer', answer);
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`hactivoverflow server is listening on port ${port}`));
